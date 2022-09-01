5cc71d96b4459473717543c6a0a249feea6acef4
Adhitya Mohan <me@adhityamohan.in>
2022-09-01 19:22:19 +0530

    sm7125-common: RealmeParts: Implement HBM switch [@marshmello61] - Port to oppo - Add inside parts instead of Settings > Display
    [maxven]
    - Add HBM QS tile
    
    Co-authored-by: maxwen <max.weninger@gmail.com>
    Co-authored-by: Mayur <ultramayur123@gmail.com>
    Signed-off-by: Mayur <ultramayur123@gmail.com>
    Signed-off-by: aminfauzi <aremean0107@gmail.com>

--- RealmeParts/src/org/aospextended/device/RealmeParts.java
+++ RealmeParts/src/org/aospextended/device/RealmeParts.java
@@ -43,8 +43,8 @@ import androidx.preference.TwoStatePreference;
 import org.aospextended.device.gestures.TouchGestures;
 import org.aospextended.device.gestures.TouchGesturesActivity;
 import org.aospextended.device.doze.DozeSettingsActivity;
-import org.aospextended.device.display.DcDimmingSettingsFragment;
-import org.aospextended.device.display.DcDimmingSettingsActivity;
+import org.aospextended.device.display.DisplaySettingsFragment;
+import org.aospextended.device.display.DisplaySettingsActivity;
 import org.aospextended.device.vibration.VibratorStrengthPreference;
 
 import java.text.DateFormat;
@@ -70,7 +70,7 @@ public class RealmeParts extends PreferenceFragment implements
 
     private Preference mDozePref;
     private Preference mGesturesPref;
-    private Preference mDCPref;
+    private Preference mDisplayPref;
     private VibratorStrengthPreference mVibratorStrength;
 
     @Override
@@ -104,15 +104,15 @@ public class RealmeParts extends PreferenceFragment implements
 
 
 
-       mDCPref = findPreference("dc_dimming");
-       mDCPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                Intent intent = new Intent(getContext(), DcDimmingSettingsActivity.class);
-                startActivity(intent);
-                return true;
-           }
-       });
+        mDisplayPref = findPreference("display_settings");
+        mDisplayPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+             @Override
+             public boolean onPreferenceClick(Preference preference) {
+                 Intent intent = new Intent(getContext(), DisplaySettingsActivity.class);
+                 startActivity(intent);
+                 return true;
+            }
+        });
 
 /*        PreferenceCategory vib_strength = (PreferenceCategory) getPreferenceScreen()
                  .findPreference("vib_strength_category");
