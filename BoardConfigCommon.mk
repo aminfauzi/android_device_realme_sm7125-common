6e5cee45f9beada1f6bdaac7905d22686dddf978
w1zzyl <w1zzyl@protonmail.com>
2022-09-11 11:12:17 +0000

    sm7125-common: Import device_framework_matrix.xml
    - From: https://github.com/LineageOS/android_vendor_lineage/blob/lineage-19.1/config/device_framework_matrix.xml

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -232,7 +232,7 @@ DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
-    vendor/lineage/config/device_framework_matrix.xml \
+    $(COMMON_PATH)/device_framework_matrix.xml \
     $(COMMON_PATH)/framework_compatibility_matrix.xml
 
 ## NFC Configuration
