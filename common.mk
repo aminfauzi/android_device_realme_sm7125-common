9b30933d71976321fd95a022a7cddd6d5e2a4e28
just_inntime <just_inntime@outlook.com>
2025-04-03 00:45:07 +0800

    sm7125-common: Introduce parts [1/2] Based on XiaomiParts for peridot Special thanks to kenway214

--- common.mk
+++ common.mk
@@ -81,7 +81,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
-## Display
+# Core Packages
+PRODUCT_PACKAGES += \
+    Parts
+
+# Display
 PRODUCT_PACKAGES += \
     disable_configstore \
     gralloc.qcom \
