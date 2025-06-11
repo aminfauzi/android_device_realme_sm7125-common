5c8d43809332d3e70c1c99734dbc6ffe5cacf85d
aminfauzi <aremean0107@gmail.com>
2025-06-11 15:39:41 +0800

    sm7125-common: Update boot-image-profile.txt path for A15 QPR2
    Signed-off-by: aminfauzi <aremean0107@gmail.com>

--- common.mk
+++ common.mk
@@ -410,7 +410,7 @@ SYSTEMUI_OPTIMIZE_JAVA := true
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
 ## Performance critical applications
 PRODUCT_DEXPREOPT_SPEED_APPS += \
