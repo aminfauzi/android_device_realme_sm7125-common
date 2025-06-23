7da8b7d648a3303b544a1cfdb6e35ae64451dace
aminfauzi <aremean0107@gmail.com>
2025-06-23 17:55:47 +0800

    sm7125-common: Switch to QTI Memtrack AIDL HAL
    Signed-off-by: aminfauzi <aremean0107@gmail.com>

--- common.mk
+++ common.mk
@@ -90,13 +90,11 @@ PRODUCT_PACKAGES += \
     libsdmcore \
     libsdmutils \
     libvulkan \
-    memtrack.atoll \
     android.hardware.graphics.composer@2.4-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.memtrack-service \
     vendor.display.config@1.5 \
     vendor.display.config@1.11.vendor \
     vendor.display.config@2.0 \
