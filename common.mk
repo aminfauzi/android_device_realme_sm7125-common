de09ea2ed6f1a26bd0f58fbee7db17075889fcfe
aminfauzi <aremean0107@gmail.com>
2025-07-13 14:39:49 +0800

    common: Stop copying Google software codec XMLs to /vendor
    The framework already installs media_codecs_google_{audio,video,video_le,
    telephony}.xml under /system/etc. Duplicating them in /vendor/etc is
    obsolete on Android 11+ and leads to duplicate‑parser warnings while
    wasting ~100 kB in vendor.img.
    
    Tested on RMX2061 (Realme 6 Pro, SM7125): clean boot, functional
    telephony IMS audio, and CTS media tests all pass without the vendor
    copies.
    
    Signed-off-by: aminfauzi <aremean0107@gmail.com>

--- common.mk
+++ common.mk
@@ -176,13 +176,9 @@ PRODUCT_PACKAGES += \
 
 ## Media
 PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xm \
     $(LOCAL_PATH)/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     $(LOCAL_PATH)/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml \
     $(LOCAL_PATH)/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
