10f0efb70e1561c02661a4cf3c72cf374134bbd6
aminfauzi <aremean0107@gmail.com>
2025-07-13 17:53:55 +0800

    drm: Drop obsolete android.hardware.drm@{1.0‑1.4}.vendor stubs
    The build system already auto‑generates vendor‑side HIDL stubs for
    DRM interfaces, and SM7125 Widevine/ClearKey blobs no longer depend
    on them now that the stack has moved to AIDL.  Removing these
    entries avoids duplicate‑module warnings and saves ~250 kB in
    vendor.img.
    
    Signed-off-by: aminfauzi <aremean0107@gmail.com>

--- common.mk
+++ common.mk
@@ -105,13 +105,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
-PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0.vendor \
-    android.hardware.drm@1.1.vendor \
-    android.hardware.drm@1.2.vendor \
-    android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.4.vendor
-
 ## Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.realme_sm7125
