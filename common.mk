07557efbd85588d7abeb00c19efb50936038e30b
aminfauzi <aremean0107@gmail.com>
2025-12-18 17:26:23 +0800

    sm7125-common: Build Calendar App
    Signed-off-by: aminfauzi <aremean0107@gmail.com>

--- common.mk
+++ common.mk
@@ -460,6 +460,10 @@ PRODUCT_PACKAGES += \
 ## Blur
 TARGET_ENABLE_BLUR := false
 
+## Calendar - lineageos
+PRODUCT_PACKAGES += \
+    Etar
+
 # Disable Scudo to save RAM.
 PRODUCT_DISABLE_SCUDO := true
 
