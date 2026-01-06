09f71b9b1a515c1e1be3d1eb1499e2150eba75a2
ozturkmutlu65 <ozturkmutlu65@gmail.com>
2026-01-06 15:55:38 +0300

    sm7125-common: Add com.android.bluetooth context to seapp_contexts
    Signed-off-by: aminfauzi <aremean0107@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -276,6 +276,7 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 # ==========================================
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # ==========================================
