a293ed73e97acbf902af464f83096df0d897078b
aminfauzi <aremean0107@gmail.com>
2026-03-11 14:54:51 +0800

    sm7125-common: Remove unused media configs copy rule
    The configs/media/odm path is empty/non-existent, so the
    copy rule is removed to clean up the build configuration.
    
    Signed-off-by: aminfauzi <aremean0107@gmail.com>

--- common.mk
+++ common.mk
@@ -255,8 +255,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xm \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media/vendor,$(TARGET_COPY_OUT_VENDOR)/etc) \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media/odm/,$(TARGET_COPY_OUT_ODM)/etc)
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/media/vendor,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 ## Minijail
 PRODUCT_PACKAGES += \
