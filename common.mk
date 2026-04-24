c4900f8bf82686b52acbec59e9a7681a7711bbf1
aminfauzi <aremean0107@gmail.com>
2026-04-24 20:32:27 +0800

    sm7125-common: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
    Issues:
    -Widevine DRM service is broken
    -A required system library is missing
    
    If Widevine fails → app can freeze at logo
    
    Solved:
    CANNOT LINK EXECUTABLE /odm/bin/hw/android.hardware.drm@1.3-service.widevine:library libprotobuf-cpp-lite-3.9.1.so not found
    
    Signed-off-by: aminfauzi <aremean0107@gmail.com>

--- common.mk
+++ common.mk
@@ -417,7 +417,8 @@ PRODUCT_PACKAGES += \
 
 ## Protobuf
 PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-3.9.1-vendorcompat
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 ## QTI FWK Detect
 PRODUCT_PACKAGES += \
