2dcde6b9dbadc9fec822ba0850d7113947fb6bb7
aminfauzi <aremean0107@gmail.com>
2026-04-24 22:32:10 +0800

    Revert "Implementation Dolby Atmos"
    This reverts commit e259a3fb96569cdb9b77778835c83fa18dc1e7b4
    This reverts commit 36ccdd105de3b386665cd7c20c8f94d41353a7a8
    This reverts commit 9480dca134da21a0848e3f0f6bb67f04d677e69d.

--- common.mk
+++ common.mk
@@ -118,9 +118,6 @@ PRODUCT_PACKAGES += \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor
 
-## Dolby
-$(call inherit-product, hardware/dolby/dolby.mk)
-
 ## DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
