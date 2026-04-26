df2ac76fc79fca0d5a26dc2a4b718e971c0098c1
aminfauzi <aremean0107@gmail.com>
2026-04-26 15:49:02 +0800

    sm7125-common: Use appropriate arch setup
    Signed-off-by: aminfauzi <aremean0107@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,7 +20,7 @@ TARGET_NO_BOOTLOADER := true
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_VARIANT := cortex-a76
+TARGET_CPU_VARIANT := generic
 TARGET_CPU_VARIANT_RUNTIME := cortex-a76
 
 ## Secondary Architecture (ARM)
@@ -28,7 +28,7 @@ TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a76
+TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
 
 # ==========================================
