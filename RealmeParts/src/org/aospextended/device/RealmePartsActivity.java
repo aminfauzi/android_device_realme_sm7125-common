8ed815a2838b3f4756b181244fc0a839ef128291
Shahid Shamim Shah <shahidshamimshah3@gmail.com>
2026-05-31 02:59:43 +0530

    sm7125-common: RealmeParts: Increase target SDK version and add icons for preferences
    Update AndroidManifest and XML resources for improved UI and functionality
    
    Signed-off-by: aminfauzi <aremean0107@gmail.com>

--- RealmeParts/src/org/aospextended/device/RealmePartsActivity.java
+++ RealmeParts/src/org/aospextended/device/RealmePartsActivity.java
@@ -15,6 +15,7 @@
  */
 package org.aospextended.device;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -37,6 +38,14 @@ public class RealmePartsActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setTitle("realme Lab");
+
+        // On Android 16 the Settings DeviceDefault theme no longer guarantees a
+        // classic ActionBar (it moved to a toolbar), so getActionBar() can be
+        // null. Guard it; an unguarded call here was crashing the screen on open.
+        final ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         Fragment fragment = getFragmentManager().findFragmentById(android.R.id.content);
         if (fragment == null) {
